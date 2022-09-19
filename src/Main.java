ccf4e8ed9f20d5787d10aeed200ac470c58ab469
Иван <ivanpaskhin@yandex.ru>
2022-09-19 19:01:05 +0300

    Задача 1

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        /// Домашнее задание - 1 ///
+
+          /// Задача 1 ///
+
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+
+        System.out.println("Dog: " + dog +" "+"Cat: " + cat +" "+"Paper: " + paper +"\n");
+    }
+}
