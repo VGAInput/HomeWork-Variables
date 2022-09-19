c3b5548a8ac3a05f9a32af0bf87bd2a0735c3a91
Иван <ivanpaskhin@yandex.ru>
2022-09-19 19:28:42 +0300

    Переменные - Задание 1

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,29 @@ public class Main {
 
         /// Домашнее задание - 1 ///
 
-          /// Задача 1 ///
+            ///  Задача 1  ///
 
         var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
 
         System.out.println("Dog: " + dog +" "+"Cat: " + cat +" "+"Paper: " + paper +"\n");
+
+            ///  Задача 2  ///
+
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+
+        System.out.println("Dog: " + dog +" "+"Cat: " + cat +" "+"Paper: " + paper +"\n");
+
+            ///  Задача 3  ///
+
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+
+        System.out.println("Dog: " + dog +" "+"Cat: " + cat +" "+"Paper: " + paper +"\n");
+
     }
 }
