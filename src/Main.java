cac2fadc4d44a044d28df6a6f8885f25976a8d21
Иван <ivanpaskhin@yandex.ru>
2022-09-27 21:17:34 +0300

    ДЗ: Условный оператор. Часть 1 (1 - 6)

--- src/Main.java
+++ src/Main.java
@@ -1,128 +1,122 @@
+import java.math.MathContext;
+
 public class Main {
     public static void main(String[] args) {
 
-        /// Домашнее задание - Переменные ч.2 ///
+        //// Домашние задания "Условный оператор ч.1" - от 1 до 6 ////
 
-            /* Задача 1 */
+        homeWork1(9);
+        homeWork1(19);
+        System.out.println();
 
-        byte byte_x = 127;
-        short short_x = 32767;
-        int int_x = 456789444;
-        long long_x = 489536312;
-        float float_x = 34.89f;
-        double double_x = 13.37;
+        homeWork2(7);
+        homeWork2(19);
+        homeWork2(25);
+        System.out.println();
 
-        System.out.println("Значение переменной byte_x с типом byte равно: " + byte_x);
-        System.out.println("Значение переменной short_x с типом short равно: " + short_x);
-        System.out.println("Значение переменной int_x с типом int равно: " + int_x);
-        System.out.println("Значение переменной long_x с типом long равно: " + long_x);
-        System.out.println("Значение переменной float_x с типом float равно: " + float_x);
-        System.out.println("Значение переменной long_x с типом long равно: " + double_x + "\n");
+        homeWork3(162);
+        homeWork3(90);
+        homeWork3(41);
+        System.out.println();
 
-            /* Задача 2 */
+        homeWork4(6);
+        homeWork4(10);
+        homeWork4(19);
+        homeWork4(25);
+        System.out.println();
 
-        float num1 = 27.12f;
-        long num2 = 987678965549L;
-        byte num3 = 2;
-        short num4 = 786;
-        boolean bool1 = false;
-        short num5 = 569;
-        short num6 = -159;
-        int num7 = 276897;
-        double num8 = 67.;
+        homeWork5(2,false);
+        homeWork5(6,false);
+        homeWork5(6,true);
+        homeWork5(15,false);
+        System.out.println();
 
-            /* Задача 3 */
-
-        short pavlovnaClass_studens = 23;
-        short sergeevnaClass_studens = 27;
-        short andreevnaClass_students = 30;
-
-        short paperSheets = 480;
-
-        int  sheetsPerStudents = paperSheets / (pavlovnaClass_studens + sergeevnaClass_studens + andreevnaClass_students);
-
-        System.out.println("На каждого ученика рассчитано по" + sheetsPerStudents + " листов бумаги\n");
-
-            /* Задача 4 */
-
-        short bottlesPerMinute = 16/2;
-
-        int bottles_20Minutes = bottlesPerMinute * 20;
-        int bottles_Day = bottlesPerMinute * 1400;
-        int bottles_3Days = bottles_Day * 3;
-        int bottles_Month = bottles_Day * 30;
-
-        System.out.println("За 20 минут работы машина произвела бутылок: " + bottles_20Minutes + " штук");
-        System.out.println("За сутки работы машина произвела бутылок: " + bottles_Day + " штук");
-        System.out.println("За 3 сутка работы машина произвела бутылок: " + bottles_3Days + " штук");
-        System.out.println("За месяц работы машина произвела бутылок: " + bottles_Month + " штук\n");
-
-            /* Задача 5 */
-
-        short paintRequired = 120;
-
-        byte paintPerClass_White = 2;
-        byte paintPerClass_Brown = 4;
-
-        int classrooms =  paintRequired / (paintPerClass_White + paintPerClass_Brown);
-
-        int paintWhite_total = classrooms * paintPerClass_White;
-        int paintBrown_total = classrooms * paintPerClass_Brown;
-
-        System.out.println("В школе, где " +classrooms+ " классов, нужно " +paintWhite_total+ " банок белой краски и "
-                +paintBrown_total+ " банок коричневой краски\n");
-
-            /* Задача 6 */
-
-        short bananaToGram = 80;
-        short milkToGram = 105;
-        short iceCreamToGram = 100;
-        short eggToGram = 70;
-
-        byte bananas = 5;
-        short milk = 200;
-        byte iceCream = 2;
-        byte eggs = 4;
-
-        float massOfBreakfastInGram = (bananas * bananaToGram) + ((milk / 100) * milkToGram) + (iceCream * iceCreamToGram) +
-                (eggs * eggToGram);
-
-        float massOfBreakfastInKilo = massOfBreakfastInGram / 1000;
-
-        System.out.println("Масса спортивного завтра в килограммах: " + massOfBreakfastInKilo + " кг.\n");
-
-            /* Задача 7 */
-
-        short looseWeightTarget = 7 * 1000;
-
-        short looseWeightMin = 250;
-        short looseWeightMax = 500;
-
-        int minimumDays = looseWeightTarget / looseWeightMax;
-        int maximumDays = looseWeightTarget / looseWeightMin;
-        int mediumAmmountOfDays = (maximumDays - minimumDays)/2 + minimumDays;
-
-        System.out.println("Минимальное время для достижение веса: " + minimumDays);
-        System.out.println("Максимальное время для достижение веса: " + maximumDays);
-        System.out.println("Среднее время для достижение веса: " + mediumAmmountOfDays + "\n");
-
-                /* Задача 8 */
-
-        int salaryMasha = 67760;
-        int salaryDenis = 83690;
-        int salaryKristina = 76230;
-
-        int newSalaryMasha = salaryMasha + (salaryMasha / 10);
-        int newSalaryDenis = salaryDenis + (salaryDenis / 10);
-        int newSalaryKristina = salaryKristina + (salaryKristina / 10);
-
-        int newYearlySalaryMasha = (newSalaryMasha / 10) * 12;
-        int newYearlySalaryDenis = (newSalaryDenis / 10) * 12;
-        int newYearlySalaryKristina = (newSalaryKristina / 10) * 12;
-
-        System.out.println("Маша теперь получает " + newSalaryMasha + " рублей. Годовой доход вырос на " + newYearlySalaryMasha+ " рублей.");
-        System.out.println("Денис теперь получает " + newSalaryDenis + " рублей. Годовой доход вырос на " + newYearlySalaryDenis+ " рублей.");
-        System.out.println("Кристина теперь получает " + newSalaryKristina + " рублей. Годовой доход вырос на " + newYearlySalaryKristina+ " рублей.");
+        homeWork6(1,2,3);
+        homeWork6(4,2,1);
+        homeWork6(5,6000,150);
+        System.out.println();
 
     }
+
+    private static void homeWork1(int inputUserAge) {
+        int userAge = inputUserAge;
+
+        if (userAge >= 18) {
+            System.out.println("Поздравляем с совершеннолетием!");
+        } else System.out.println("Вы ещё не достигли совершеннолетия, надо подождать.");
+    }
+
+    private static void homeWork2(int inputUserAge) {
+        int userAge = inputUserAge;
+
+        if (userAge >= 7 && userAge < 18) {
+            System.out.println("Пользователь ходит в школу.");
+        } else if (userAge >= 18 && userAge < 24) {
+            System.out.println("Пользователь закончил школу и идёт в университет.");
+        } else if (userAge >= 24) {
+            System.out.println("Пользователь закончил университет и ищет первую работу.");
+        }
+
+    }
+
+    private static void homeWork3(int passengers) {
+
+        int trainTotalCapacity = 102;
+        int trainSittingPlaces = 60;
+        int trainStandingPositions = trainTotalCapacity - trainSittingPlaces;
+
+        if (passengers < trainSittingPlaces) {
+            System.out.println("В вагоне есть свободное сиденье.");
+        } else if (passengers >= trainSittingPlaces && passengers < trainTotalCapacity) {
+            System.out.println("В вагоне нет свободных сидений, но место ещё есть.");
+        } else if (passengers >= trainTotalCapacity) {
+            System.out.println("В вагоне места не осталось.");
+        }
+    }
+
+    private static void homeWork4(int userAge) {
+
+        if (userAge >= 2 && userAge <= 6) {
+            System.out.println("Если возраст человека равен " + userAge + ", то ему нужно ходить в детский сад.");
+        } else if (userAge >= 7 && userAge <= 18) {
+            System.out.println("Если возраст человека равен " + userAge + ", то ему нужно ходить в школу.");
+        } else if (userAge >= 18 && userAge < 24) {
+            System.out.println("Если возраст человека равен " + userAge + ", то ему нужно идти в университет.");
+        } else if (userAge >= 24) {
+            System.out.println("Если возраст человека равен " + userAge + ", то ему нужно идти на работу.");
+        }
+
+    }
+
+    private static void homeWork5(int userAge, boolean withParents) {
+
+        if (userAge < 5) {
+            System.out.println("Ребёнку кататься нельзя.");
+        } else if (userAge >= 5 && userAge <= 14) {
+            if (withParents)
+                System.out.println("Ребёнку можно кататься с родителями.");
+            else
+                System.out.println("Ребёнку без родителей кататься нельзя.");
+        } else if (userAge > 14) {
+            System.out.println("Ребёнку можно кататься одному.");
+        }
+
+    }
+
+    private static void homeWork6(int inputOne,int inputTwo,int inputThree){
+        int one = inputOne;
+        int two = inputTwo;
+        int three = inputThree;
+
+        if (one >= two && one >= three){
+            System.out.println(one + " - самое большое число");
+        }
+        else if (two >= one && two >= three){
+            System.out.println(two + " - самое большое число");
+        }
+        else if (three >= one && three >= two){
+            System.out.println(three + " - самое большое число");
+        }
+    }
+
 }
